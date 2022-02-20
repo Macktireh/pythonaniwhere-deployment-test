c5e92d7fb17991951c3fc898cf5654f221c770b0
Macktireh <abdimack97@gmail.com>
2022-02-20 20:55:46 +0100

    email

--- src/apps/portfolio/views.py
+++ src/apps/portfolio/views.py
@@ -68,7 +68,7 @@ def contact(request):
                 subject_content,
                 body,
                 f'{EMAIL_HOST_USER}',
-                'charmakepirlo@gmail.com'
+                ['charmakepirlo@gmail.com']
             )
             
             form.save()
